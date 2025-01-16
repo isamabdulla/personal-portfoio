a26994dc08ef6891f88f069782b803d9dee3fd4d
isamabdulla <isamabdulla19@gmail.com>
2025-01-16 15:24:50 +0530

    isaam

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,22 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          Hi, I’m ISAM RV, a passionate Full Stack Web Developer with expertise in building dynamic, scalable, and user-friendly web applications. With a strong foundation in the MERN stack (MongoDB, Express.js, React, and Node.js), I create seamless solutions that meet modern web standards.
+          </p>
+          <p className="hero--section-description">
+          I’m driven by curiosity, creativity, and the desire to solve real-world problems through technology. Whether it’s building a responsive website or designing an intuitive user interface, I’m committed to delivering excellence in every project.
+
+
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm ISAM RV</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+          Turning Ideas into Reality with Code and Creativity
+            <br />A Developer Who Designs, A Designer Who Codes
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
